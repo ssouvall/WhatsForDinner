c78e681ae623916189baf9ac36846c7c50c9cb4e
ssouvall <stephensouvall@gmail.com>
2021-11-06 10:04:56 -0600

    Add category migration

--- Domain/Recipe.cs
+++ Domain/Recipe.cs
@@ -7,6 +7,7 @@ namespace Domain
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Category { get; set; }
         public string Description { get; set; }
         public string Instructions { get; set; }
         
---
+++ Persistence/Migrations/20211106160200_AddRecipeCategory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class AddRecipeCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Recipes",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Recipes");
+        }
+    }
+}
